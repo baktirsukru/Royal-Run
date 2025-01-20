126a302909e4425283578ab180c02f104aa4d40a
baktirsukru <101118829+baktirsukru@users.noreply.github.com>
2025-01-20 15:20:55 +0300

    collision cooldown

--- Assets/Scripts/Player/Player Collision Handler.cs
+++ Assets/Scripts/Player/Player Collision Handler.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown = 1f; 
+
+    const string hitString = "Hit";
+    float cooldownTimer = 0f;
+    
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return;
+
+        animator.SetTrigger(hitString);
+        cooldownTimer = 0f;
     }
 }
